d542040b92b9b2ba075beeab4a5a75dd7bc2f4c4
esim7 <makishev@yahoo.com>
2020-02-12 13:21:15 +0600

    Клиентская часть

---
+++ WChecker.Domain/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace WChecker.Domain
+{
+    public class User : Entity
+    {
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string Position { get; set; }
+        public string FullName { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
